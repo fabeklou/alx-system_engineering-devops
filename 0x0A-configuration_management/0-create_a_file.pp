440b5c0ad82e07370215cf6390099d5b3f8a651f
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-24 22:47:59 +0000

    feat: create a file with content using puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,12 @@
+# This manifest creates a <school> file in the </tmp>
+#     directory with the string 'I love puppet' as
+#     content
+
+$file_path = '/tmp/school'
+
+file { $file_path:
+  content => 'I love puppet',
+  owner   => www-data,
+  group   => www-data,
+  mode    => '0744',
+}
