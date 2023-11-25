6de1e44bd76f6ae3d31491a76015de29024cc25c
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-25 02:28:35 +0000

    feat: install flask and werkzeug using exec

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,10 @@
 # This manifest installs a version <2.1.0> of the
 #     the python framework <flask> using <pip3>
 
-$pckg = 'flask'
+exec {'flask':
+    command => '/bin/pip3 install flask==2.1.0'
+}
 
-package { $pckg:
-    ensure   => '2.1.0',
-    provider => 'pip3'
+exec {'werkzeug':
+    command => '/bin/pip3 install werkzeug==2.1.1'
 }
