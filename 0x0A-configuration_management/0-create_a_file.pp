a991d2778ef6746ec61f8c7156c517173dc3f617
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-25 00:57:10 +0000

    fix: change 0744 to u=rwx,g=r,o=r

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # This manifest creates a <school> file in the </tmp> directory
+
 file { '/tmp/school':
     ensure  => 'file',
     owner   => 'www-data',
     group   => 'www-data',
-    mode    => '0744',
+    mode    => 'u=rwx,g=r,o=r',
     content => 'I love puppet',
 }
