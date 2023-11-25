dcf631c9d782e980e5163c44d622b64abf544f9e
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-11-25 00:44:10 +0000

    refactor: rm varibale

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,6 @@
-# This manifest installs a version <2.1.0> of the
-#     the python framework <flask> using <pip3>
+# This manifest installs version <2.1.0> of <flask> using <pip3>
 
-$pckg = 'flask==2.1.0'
-
-package { $pckg:
+package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
     name     => 'flask'
