8d137103ce1e76f90b915d3bcbfafd9060dae537
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-12-20 01:45:03 +0000

    feat: create a custom HTTP header response using puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,46 @@
+# This manifest install and configure an Nginx server
+
+$config_file = '/etc/nginx/sites-available/default'
+$redirect_me = "    # Managed by Puppet -V 5.5
+    location /redirect_me {
+        return 301 https://github.com/fabeklou;
+    }
+"
+$custom_header = "add_header X-Served-By \"${hostname}\";"
+
+exec { 'Update : Advanced Packaging Tool':
+    command     => '/bin/apt update -y'
+}
+
+package { 'Installing Nginx -V 1.18.0':
+    ensure   => installed,
+    name     => 'nginx',
+    provider => 'apt'
+}
+
+exec { 'Allowing nginx to accept http requests on port 80':
+    command     => '/sbin/ufw allow \'Nginx HTTP\'',
+}
+
+file { '/var/www/html/index.html':
+    ensure  => present,
+    content => 'Hello World!'
+}
+
+file_line { 'Add redirection context to the default config file':
+    ensure => present,
+    path   => $config_file,
+    line   => $redirect_me,
+    after  => 'server_name _;'
+}
+
+file_line { 'creating a custom HTTP header response':
+    ensure => present,
+    path   => $config_file,
+    line   => $custom_header,
+    after  => 'server_name _;'
+}
+
+exec { 'restarting the nginx server':
+    command     => '/sbin/service nginx restart'
+}
