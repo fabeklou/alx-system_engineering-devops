c9afa034b8b81e82d2c18efa844775114b4b3040
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-12-20 12:04:15 +0000

    fix: use arrow to denote relationships

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,49 +2,27 @@
 # costum http header response
 
 $config_file = '/etc/nginx/sites-available/default'
-$redirect_me = "    # Managed by Puppet -V 5.5
-    location /redirect_me {
-        return 301 https://github.com/fabeklou;
-    }
-"
+
 $custom_header = "
-    http {
-        add_header X-Served-By \"${hostname}\";
+    add_header X-Served-By \"${hostname}\";
 "
 
 exec { 'Update : Advanced Packaging Tool':
     command     => '/bin/apt update -y'
 }
 
-package { 'Installing Nginx -V 1.18.0':
-    ensure   => installed,
-    name     => 'nginx',
-    provider => 'apt'
+-> package { 'Ensure Nginx -V 1.18.0 is installed':
+    ensure => present,
+    name   => 'nginx',
 }
 
-exec { 'Allowing nginx to accept http requests on port 80':
-    command     => '/sbin/ufw allow \'Nginx HTTP\'',
-}
-
-file { '/var/www/html/index.html':
-    ensure  => present,
-    content => 'Hello World!'
-}
-
-file_line { 'Add redirection context to the default config file':
+-> file_line { 'creating a custom HTTP header response':
     ensure => present,
     path   => $config_file,
-    line   => $redirect_me,
-    after  => 'server_name _;'
-}
-
-file_line { 'creating a custom HTTP header response':
-    ensure => present,
-    path   => $config_file,
-    match  => 'http {',
+    after  => 'index index.html index.htm index.nginx-debian.html;',
     line   => $custom_header
 }
 
-exec { 'restarting the nginx server':
-    command     => '/sbin/service nginx restart'
+-> exec { 'restarting the nginx server':
+    command    => '/sbin/service nginx restart'
 }
