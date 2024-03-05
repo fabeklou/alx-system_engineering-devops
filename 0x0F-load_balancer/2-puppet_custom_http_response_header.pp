187564840e4db6cb1aa57670f6e7430dc185e7f4
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-03-05 02:30:33 +0000

    fix: execution order

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -16,36 +16,36 @@ node default {
         command => '/bin/apt update -y'
     }
 
-    package { 'Installing Nginx -V 1.18.0':
+    -> package { 'Installing Nginx -V 1.18.0':
         ensure   => installed,
         name     => 'nginx',
         provider => 'apt'
     }
 
-    exec { 'Allowing nginx to accept http requests on port 80':
+    -> exec { 'Allowing nginx to accept http requests on port 80':
         command => '/sbin/ufw allow \'Nginx HTTP\'',
     }
 
-    file { '/var/www/html/index.html':
+    -> file { '/var/www/html/index.html':
         ensure  => present,
         content => 'Hello World!'
     }
 
-    file_line { 'Add redirection context to the default config file':
+    -> file_line { 'Add redirection context to the default config file':
         ensure => present,
         path   => $config_file,
         line   => $redirect_me,
         after  => 'server_name _;'
     }
 
-    file_line { 'Add header to /etc/nginx/site-available/default':
+    -> file_line { 'Add header to /etc/nginx/site-available/default':
         ensure => present,
         path   => $config_file,
         line   => $served_by,
         after  => 'server_name _;'
     }
 
-    exec { 'restarting the nginx server':
+    -> exec { 'restarting the nginx server':
         command => '/sbin/service nginx restart'
     }
 }
