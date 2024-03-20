0d701ed446b2599686ae8a0e016d5b78eb3aacfe
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-03-20 22:38:21 +0000

    feat: make sure replication is working

---
+++ 0x14-mysql/4-test_replication.sql
@@ -0,0 +1,6 @@
+INSERT INTO tyrell_corp.nexus6 (name, email)
+VALUES ('avocado', 'avocado@food.com'),
+    ('oil', 'oil@food.com'),
+    ('salt', 'salt@food.com');
+SELECT *
+FROM tyrell_corp.nexus6;
