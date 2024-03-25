c7264ee43b9f781d093e6d5cab55e16e53d3430e
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-03-25 21:14:18 +0000

    feat: get user data and save it to a csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""This module sends a GET request to a REST API and
+prints the data, formated to the stdin
+"""
+
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    USER_ID: str = sys.argv[1]
+
+    USER_INFO_URL: str = "https://jsonplaceholder.typicode.com/users/{}"\
+        .format(USER_ID)
+    TODOS_URL: str = "https://jsonplaceholder.typicode.com/todos?userId={}"\
+        .format(USER_ID)
+    user_info = requests.get(USER_INFO_URL).json()
+    tasks = requests.get(TODOS_URL).json()
+
+    USERNAME: str = user_info.get("username")
+
+    with open("{}.csv".format(USER_ID), "w", newline="") as csvfile:
+        fieldnames = ["USER_ID", "USERNAME",
+                      "TASK_COMPLETED_STATUS", "TASK_TITLE"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for task in tasks:
+            writer.writerow({"USER_ID": USER_ID, "USERNAME": USERNAME,
+                             "TASK_COMPLETED_STATUS": task.get("completed"),
+                             "TASK_TITLE": task.get("title")})
