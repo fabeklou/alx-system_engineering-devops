2c7e524ce1c60331adbfef701c5e81faf69bf8e6
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-04-09 23:05:55 +0000

    fix: function prototype

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,22 +5,22 @@
 import requests
 
 
-def number_of_subscribers(sub):
+def number_of_subscribers(subreddit):
     """GET and return the number of subscribers of the given subreddit
 
     Args:
-        sub (str): the name of subreddit
+        subreddit (str): the name of subreddit
 
     Returns:
         (int): the number of subscribers of the given subreddit
             or 0 if the subreddit does not exist
     """
 
-    if not sub or not isinstance(sub, str):
+    if not subreddit or not isinstance(subreddit, str):
         return 0
 
     UA = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    URL = 'https://www.reddit.com/r/{}/about.json'.format(sub)
+    URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
     res = requests.get(URL, allow_redirects=False, headers=UA)
 
