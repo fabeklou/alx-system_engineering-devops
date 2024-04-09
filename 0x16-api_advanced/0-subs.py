d41760b64bc9e79760a95e7e588a8a3b7761fc0c
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-04-09 22:33:47 +0000

    fix: add function documentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def number_of_subscribers(sub):
+    """GET and return the number of subscribers of the given subreddit
+
+    Args:
+        sub (str): the name of subreddit
+
+    Returns:
+        (int): the number of subscribers of the given subreddit
+            or 0 if the subreddit does not exist
+    """
     import requests
 
     URL = 'https://www.reddit.com/r/{}/about.json'.format(sub)
