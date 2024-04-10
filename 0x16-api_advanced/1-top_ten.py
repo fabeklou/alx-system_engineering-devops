01c058ca2b357f1d33e84008da773d382dc2c5b6
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-04-10 01:31:20 +0000

    feat: show top 10 posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+this module contains a function that GET data from the Reddit Api
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    GET and prints the first 10 hot posts listed for a given subreddit
+
+    Args:
+        subreddit (str): the name of subreddit
+
+    """
+
+    UA = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    PARAMS = {'limit': 9}
+
+    res = requests.get(URL, headers=UA, allow_redirects=False, params=PARAMS)
+
+    if res.status_code != 200:
+        print('None')
+        return
+    for post in res.json().get('data').get('children'):
+        print(post.get('data').get('title'))
