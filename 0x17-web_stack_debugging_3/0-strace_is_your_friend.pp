e42d3ec1cc55b03a866ee8b91a3ddf641e40664e
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-04-11 18:39:19 +0000

    fix: use the exec resource

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,5 @@
 # This manifest fix 500Error returned by Apache web server
 
-$file_path = '/var/www/html/wp-settings.php'
-
-file { 'fix typo in WP config file':
-    ensure  => file,
-    path    => $file_path,
-    content => inline_template("<%= @file_content.gsub('phpp', 'php') %>"),
+exec { 'fix typo in WP config file':
+    command => "/usr/sbin/sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php"
 }
