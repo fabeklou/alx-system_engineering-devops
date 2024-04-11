9d1de4dc8848b81a165fcb2bf12a18af4264202c
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2024-04-11 18:44:56 +0000

    fix: add paths for the sed command

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # This manifest fix 500Error returned by Apache web server
 
 exec { 'fix typo in WP config file':
-    command => "/usr/sbin/sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php"
+    path    => ['/usr/sbin', '/sbin', '/usr/bin', '/bin'],
+    command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php"
 }
